196f35a4219331f09b187f71c4f019d02b8fd32a
kennymen <kfloresc@ulasalle.edu.pe>
2025-03-29 01:20:54 -0500

    solucionando error de main

--- backend_server.py
+++ backend_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "panconwebito" 
 
 jwt = JWTManager(app)
@@ -12,5 +12,5 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(port=5001, debug=True)
